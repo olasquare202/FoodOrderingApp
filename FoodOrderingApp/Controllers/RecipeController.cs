01875a4933b4584e074c94346dd4dd0ecd0d8fba
chrisshola1 <tkwalks1@gmail.com>
2025-01-03 05:50:43 +0100

    Add project files.

---
+++ FoodOrderingApp/Controllers/RecipeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodOrderingApp.Controllers
+{
+    public class RecipeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FoodOrderingApp/README.txt
@@ -0,0 +1,8 @@
+﻿Online Food Order Application using ASP.NET Core MVC with SQL Server DB
+Project Requirements in description.
+Steps:
+1) Create Project in Visual Studio 2022.
+2) Menubar
+3) Carousel on Home Page
+4) Footer
+5) Recipe Details
